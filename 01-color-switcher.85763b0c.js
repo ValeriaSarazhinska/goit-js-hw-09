02c76ac406e04f23f6721326ef3cc8d3b79b69ea
ValeriaSarazhinska <109595524+ValeriaSarazhinska@users.noreply.github.com>
2023-01-06 13:10:44 +0000

    Deploying to gh-pages from @ ValeriaSarazhinska/goit-js-hw-09@320ee7f0d15b24f89fd591da31229e84db6ae330 🚀

---
+++ 01-color-switcher.85763b0c.js
@@ -0,0 +1,2 @@
+const e=document.querySelector("[data-start]"),t=document.querySelector("[data-stop]"),r=document.querySelector("body");let a;e.addEventListener("click",(()=>{e.disabled=!0,a=setInterval((()=>{r.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3)})),t.addEventListener("click",(()=>{e.disabled=!1,clearInterval(a)}));
+//# sourceMappingURL=01-color-switcher.85763b0c.js.map
